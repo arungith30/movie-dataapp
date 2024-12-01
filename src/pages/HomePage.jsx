12ddd0dffca0a207e658a6b13ef2e36f757f9c99
arun <arunks002info@gmail.com>
2024-12-01 14:54:47 +0530

    chech dotenv config

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import "./homepage.css";
 import { useState, React } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 const HomePage = () => {
   const [searchInput, setSearchInput] = useState("");
