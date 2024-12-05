6d1b37cf0f1ddc17af7ae00f36c81f5dcf5bd596
arun <arunks002info@gmail.com>
2024-12-05 16:30:26 +0530

    favorite page added

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,7 +1,36 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  const localData = JSON.parse(localStorage.getItem("favorites")) || [];
+  console.log(localData);
+  return (
+    <>
+      {localData.length > 0 ? (
+        <div className="movies">
+          {localData.map((movie) => (
+            <div key={movie.imdbID} className="movie">
+              <Link to={`/movies/${movie.imdbID}`}>
+                <img
+                  src={
+                    movie.Poster !== "N/A"
+                      ? movie.Poster
+                      : "https://via.placeholder.com/150"
+                  }
+                  alt={`${movie.Title} poster`}
+                />{" "}
+                <h3>{movie.Title}</h3>
+              </Link>
+              <p>{movie.Year}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No favorites yet</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default FavoritesPage;
